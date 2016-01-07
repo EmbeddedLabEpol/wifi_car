81212c581e9da44110bf07525a0749892a68d444
EmbeddedLabEpol <EmbeddedLabEpol@users.noreply.github.com>
2016-01-07 13:17:01 +0100

    Update auto.ino

--- ARDUINO/auto.ino
+++ ARDUINO/auto.ino
@@ -1,118 +1,136 @@
-#include <IRremote.h>
- 
+
 #include "engine.hpp"
+#define LED_RED 3
+#define LED_GREEN 2
 
-
-#define przodu 0xE0E006F9
-#define tylu   0xE0E08679
-#define lewo    0xE0E0A659
-#define prawo   0xE0E046B9
-#define _STOP  0xE0E016E9
-#define w_PRAWO 0xE0E0E01F
-#define w_LEWO   0xE0E048B7
-
-#define szybko 0xE0E008F7
-#define wolno  0xE0E0D02F
-
-
-
-#define irPin 11
-
-
-IRrecv irrecv(irPin);
-decode_results results;
 volatile unsigned int speed_car = 0;
 Engine motor;
-
+unsigned long led_time = 0;
+String command;
+String value;
+;
 
 void setup() {
   Serial.begin(9600);
-  irrecv.enableIRIn();
+
   Serial.print ("dziala");
-  motor.init(10,9,8,7,6,5);
+  motor.init(10, 9, 8, 7, 6, 5);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
 }
 
-
+void led_blink() {
+  if (millis() - led_time < 500 ) {
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_GREEN, LOW);
+    //Serial.println("GREEN");
+  }
+  else if (millis() - led_time < 1000 && millis() - led_time > 499)
+  {
+    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_GREEN, HIGH);
+    // Serial.println("RED");
+  }
+  else
+  {
+    led_time = millis();
+  }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop() {
-  if (irrecv.decode(&results)) {
-    Serial.print("0x");
-    Serial.println(results.value, HEX);
-    delay(50);
-    if (results.value == przodu)
+  if (Serial.available() > 6) {
+
+
+   // Serial.println("START");
+    command =  Serial.readStringUntil(':');
+
+    //Serial.println(command);
+
+    value =  Serial.readStringUntil(';');
+    speed_car = value.toInt();
+    delay(10);
+    while (Serial.available()) {
+      Serial.read();
+    }
+
+    if (command == "forward" )
     {
-      Serial.println("przedu");
+
+      
       motor.go_forward();
       motor.set_power(speed_car);
       Serial.println(speed_car);
+      Serial.println("OK");
     }
 
-else if (results.value == szybko)
+    else if (command == "fast" )
     {
-      Serial.println("szybko");
-      if (speed_car < 245){
-      speed_car+=10;
-      }
-       Serial.println(speed_car);
-       motor.set_run_time(millis());
-    }
-
-else if (results.value == wolno)
-    {
-      Serial.println("wolno");
-     if ( speed_car > 20){
-      speed_car-=10;
-     }
-     Serial.println(speed_car);
-     motor.set_run_time(millis());
-    }
-
-
-else if (results.value == w_LEWO)
-    {
-      Serial.println("wlewo");
-      motor.turn_left(speed_car/2);
       
+      if (speed_car < 245) {
+        speed_car += 10;
+      }
+      Serial.println("OK");
+      motor.set_run_time(millis());
     }
 
-else if (results.value == w_PRAWO)
+    else if (command == "slow")
     {
-      Serial.println("w_prawo");
-     motor.turn_right(speed_car/2);
+       
+      if ( speed_car > 20) {
+        speed_car -= 10;
+      }
+      Serial.println("OK");
+      motor.set_run_time(millis());
+    }
+
+
+    else if (command == "t_left")
+    {
+      Serial.println("OK");
+      motor.turn_left(  speed_car);
+
+    }
+
+    else if (command == "t_right")
+    {
+      Serial.println("OK");
+      motor.turn_right(speed_car);
     }
 
 
 
-    
-    else if (results.value == tylu)
+
+    else if (command == "back")
     {
-      Serial.println("tylu");
-     motor.go_back();
-     motor.set_power(speed_car);
-     Serial.println(speed_car);
+       
+      motor.go_back();
+      motor.set_power(speed_car);
+      Serial.println("OK");
+       
     }
-    else if (results.value == _STOP)
+    else if (command == "STOP")
     {
-      Serial.println("stop");
-     motor.hard_stop();
+       
+      motor.hard_stop();
+      Serial.println("OK");
     }
-    else if (results.value == lewo)
+    else if (command == "left" )
     {
-      Serial.println("lewo");
-     motor.rotation_left();
-     motor.set_power(250);
+       
+      motor.rotation_left();
+      motor.set_power(speed_car);
+      Serial.println("OK");
     }
-    else if (results.value == prawo)
+    else if ( command == "right")
     {
-      Serial.println("prawo");
+       
       motor.rotation_right();
-      motor.set_power(250);
+      motor.set_power(speed_car);
+      Serial.println("OK");
     }
 
-
-
-   
-    irrecv.resume();
   }
-
-  motor.timeout(2000);
+  led_blink();
+  motor.timeout(500);
 }
+
